31513588c279f655062790a839f10eb05afdd251
olsenw <olsenw@onid.oregonstate.edu>
2016-02-10 00:01:39 -0800

    Revert "disable the wave watch views"
    This reverts commit 983f1c2ed13089c1c492c4416462ae8547eacb54.

--- SharkEyesCore/views.py
+++ SharkEyesCore/views.py
@@ -32,8 +32,8 @@ def home(request):
     # you add new models.
     # TODO: add 7 back in if you want to add in the wave period model
 
-    #num_defs = len(OverlayDefinition.objects.filter(is_base=True, id__in=[1,3,4, 6]))
-    num_defs = len(OverlayDefinition.objects.filter(is_base=True, id__in=[1,3]))
+    num_defs = len(OverlayDefinition.objects.filter(is_base=True, id__in=[1,3,4, 6]))
+    #num_defs = len(OverlayDefinition.objects.filter(is_base=True, id__in=[1,3]))
 
     list_of_times = datetimes[:len(datetimes)/num_defs]
 
@@ -44,8 +44,8 @@ def home(request):
     #context = {'overlays': overlays_view_data, 'defs': OverlayDefinition.objects.filter(is_base=True).exclude(id=4), 'times':list_of_times, 'windoverlays': wind_overlays_view_data, 'winddefs': OverlayDefinition.objects.filter(id=5), 'windtimes':list_of_wind_times}
 
     # TODO: add 7 back in if you want to add in the wave period model
-    #context = {'overlays': overlays_view_data, 'defs': OverlayDefinition.objects.filter(is_base=True, id__in=[1,3,4, 6]), 'times':list_of_times }
-    context = {'overlays': overlays_view_data, 'defs': OverlayDefinition.objects.filter(is_base=True, id__in=[1,3]), 'times':list_of_times }
+    context = {'overlays': overlays_view_data, 'defs': OverlayDefinition.objects.filter(is_base=True, id__in=[1,3,4, 6]), 'times':list_of_times }
+    #context = {'overlays': overlays_view_data, 'defs': OverlayDefinition.objects.filter(is_base=True, id__in=[1,3]), 'times':list_of_times }
 
     return render(request, 'index.html', context)
 
