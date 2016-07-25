b6db7cd8d19ff9913901a0e464dcc66bf4089044
seacastdev <seacastdev@gmail.com>
2016-07-25 16:55:07 -0700

    more migrations

---
+++ SharkEyesCore/migrations/0010_auto__add_field_feedbackhistory_sent__add_field_feedbackhistory_feedba.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'FeedbackHistory.sent'
+        db.add_column(u'SharkEyesCore_feedbackhistory', 'sent',
+                      self.gf('django.db.models.fields.BooleanField')(default=False),
+                      keep_default=False)
+
+        # Adding field 'FeedbackHistory.feedback_name'
+        db.add_column(u'SharkEyesCore_feedbackhistory', 'feedback_name',
+                      self.gf('django.db.models.fields.CharField')(default='', max_length=2000, blank=True),
+                      keep_default=False)
+
+        # Adding field 'FeedbackHistory.feedback_email'
+        db.add_column(u'SharkEyesCore_feedbackhistory', 'feedback_email',
+                      self.gf('django.db.models.fields.CharField')(default='', max_length=2000, blank=True),
+                      keep_default=False)
+
+        # Adding field 'FeedbackHistory.feedback_phone'
+        db.add_column(u'SharkEyesCore_feedbackhistory', 'feedback_phone',
+                      self.gf('django.db.models.fields.CharField')(default='', max_length=2000, blank=True),
+                      keep_default=False)
+
+        # Adding field 'FeedbackHistory.feedback_date'
+        db.add_column(u'SharkEyesCore_feedbackhistory', 'feedback_date',
+                      self.gf('django.db.models.fields.CharField')(default=datetime.datetime(2016, 7, 25, 0, 0), max_length=100),
+                      keep_default=False)
+
+        # Adding field 'FeedbackQuestionaire.sent'
+        db.add_column(u'SharkEyesCore_feedbackquestionaire', 'sent',
+                      self.gf('django.db.models.fields.BooleanField')(default=False),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'FeedbackHistory.sent'
+        db.delete_column(u'SharkEyesCore_feedbackhistory', 'sent')
+
+        # Deleting field 'FeedbackHistory.feedback_name'
+        db.delete_column(u'SharkEyesCore_feedbackhistory', 'feedback_name')
+
+        # Deleting field 'FeedbackHistory.feedback_email'
+        db.delete_column(u'SharkEyesCore_feedbackhistory', 'feedback_email')
+
+        # Deleting field 'FeedbackHistory.feedback_phone'
+        db.delete_column(u'SharkEyesCore_feedbackhistory', 'feedback_phone')
+
+        # Deleting field 'FeedbackHistory.feedback_date'
+        db.delete_column(u'SharkEyesCore_feedbackhistory', 'feedback_date')
+
+        # Deleting field 'FeedbackQuestionaire.sent'
+        db.delete_column(u'SharkEyesCore_feedbackquestionaire', 'sent')
+
+
+    models = {
+        u'SharkEyesCore.feedbackhistory': {
+            'Meta': {'object_name': 'FeedbackHistory'},
+            'feedback_comments': ('django.db.models.fields.CharField', [], {'max_length': '2000'}),
+            'feedback_date': ('django.db.models.fields.CharField', [], {'default': 'datetime.datetime(2016, 7, 25, 0, 0)', 'max_length': '100'}),
+            'feedback_email': ('django.db.models.fields.CharField', [], {'max_length': '2000', 'blank': 'True'}),
+            'feedback_name': ('django.db.models.fields.CharField', [], {'max_length': '2000', 'blank': 'True'}),
+            'feedback_phone': ('django.db.models.fields.CharField', [], {'max_length': '2000', 'blank': 'True'}),
+            'feedback_title': ('django.db.models.fields.CharField', [], {'max_length': '2000'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'sent': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        u'SharkEyesCore.feedbackquestionaire': {
+            'Meta': {'object_name': 'FeedbackQuestionaire'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'sent': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'ss_currents_accuracy': ('django.db.models.fields.CharField', [], {'max_length': '4'}),
+            'ss_temperature_accuracy': ('django.db.models.fields.CharField', [], {'max_length': '4'}),
+            'usage_comment': ('django.db.models.fields.CharField', [], {'max_length': '2000'}),
+            'usage_comparison': ('django.db.models.fields.CharField', [], {'max_length': '2000'}),
+            'usage_device': ('django.db.models.fields.CharField', [], {'max_length': '4'}),
+            'usage_frequency': ('django.db.models.fields.CharField', [], {'max_length': '4'}),
+            'usage_likes': ('django.db.models.fields.CharField', [], {'max_length': '2000'}),
+            'usage_location': ('django.db.models.fields.CharField', [], {'max_length': '4'}),
+            'usage_model_suggestion': ('django.db.models.fields.CharField', [], {'max_length': '2000'}),
+            'usage_suggestion': ('django.db.models.fields.CharField', [], {'max_length': '2000'}),
+            'wave_accuracy': ('django.db.models.fields.CharField', [], {'max_length': '4'}),
+            'wind_accuracy': ('django.db.models.fields.CharField', [], {'max_length': '4'})
+        }
+    }
+
+    complete_apps = ['SharkEyesCore']
