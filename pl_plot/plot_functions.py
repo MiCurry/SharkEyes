51be7c94b3460646a0b44f3cb0d9296d6e96ed74
Anthony Dunaway <anthony.dunaway@gmail.com>
2018-03-15 19:22:49 -0700

    added dst modification

--- pl_plot/plot_functions.py
+++ pl_plot/plot_functions.py
@@ -658,7 +658,6 @@ def t_cline(ax, data_file, bmap, key_ax, time_index, downsample_ratio):
     labels = ["%.1f" % num for num in float_labels]
     cbar.ax.xaxis.set_ticks(locations)
     cbar.ax.xaxis.set_ticklabels(labels)
-    d = u"\u00b0"
     cbar.set_label("Depth in Feet where the water is 2 deg F cooler than the surface")
 
 
