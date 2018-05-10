899de572661a409185a9f4792184af632a82c8fa
Miles Curry <currymiles@gmail.com>
2018-05-10 10:05:52 -0700

    clean up

--- pl_plot/models.py
+++ pl_plot/models.py
@@ -217,10 +217,8 @@ class OverlayManager(models.Manager):
         file_ids = []
         file_ids.append(DataFileManager.get_next_few_datafiles_of_a_type('NCDF')) # OSU ROMS
         file_ids.append(DataFileManager.get_next_few_datafiles_of_a_type('T-CLINE')) # T-Cline
-        file_ids.append(DataFileManager.get_next_few_datafiles_of_a_type('WAVE')) # OSU WW3
+        file_ids.append(DataFileManager.get_next_few_datafiles_of_a_type('NCEP')) # Wave Forecast
         file_ids.append(DataFileManager.get_next_few_datafiles_of_a_type('WIND')) # Wind
-        file_ids.append(DataFileManager.get_next_few_datafiles_of_a_type('NCEP')) # NCEP
-        file_ids.append(DataFileManager.get_next_few_datafiles_of_a_type('HYCOM')) # NAVY HYCOM
 
         file_ids = [item for sublist in file_ids for item in sublist] # Unravel lists of lists
 
